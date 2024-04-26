fbfdeadd213e6b2646ba1733962ac7fb82e22320
SantiagoU24 <139786524+SantiagoU24@users.noreply.github.com>
2024-04-25 22:17:24 -0500

    git commit -m "ENRUTAMIENTO BÁSICO"

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ blueprint = Blueprint('blueprint',__name__)
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-#HOME
+#HOME --
 @app.route('/')
 def home():
     return render_template('homepage.html')
